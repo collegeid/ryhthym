b1564f657ac0e109f60af2a5887ab32336bb2010
siswoadinugrh <siswoohow27@gmail.com>
2024-04-11 18:15:38 +0700

    feat: carosel music

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,68 @@
 
 // Siswo Adi Nugroho bertanggung jawab untuk bagian Music
 // Branch: MUSIC
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: '-28',
+  speed:1200,
+  autoplay: 
+    {
+      delay: 2000,
+    },
+  loop: true,
+  zoom: true,
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 80,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+var appendNumber = 4;
+var prependNumber = 1;
+document
+  .querySelector(".prepend-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide([
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+    ]);
+  });
+document
+  .querySelector(".prepend-slide")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide(
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+    );
+  });
+document
+  .querySelector(".append-slide")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.appendSlide(
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+    );
+  });
+document
+  .querySelector(".append-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.appendSlide([
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+    ]);
+  });
 
 
 
