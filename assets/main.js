43faed5af7344943faacee1a64e890e75c5a8bbd
siswoadinugrh <siswoohow27@gmail.com>
2024-04-11 22:49:01 +0700

    feat: fix sidebar and adjust content slider outoplay

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,7 @@
 
 // Siswo Adi Nugroho bertanggung jawab untuk bagian Music
 // Branch: MUSIC
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   centeredSlides: true,
   spaceBetween: '-28',
@@ -34,44 +34,6 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
-var appendNumber = 4;
-var prependNumber = 1;
-document
-  .querySelector(".prepend-2-slides")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper.prependSlide([
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-    ]);
-  });
-document
-  .querySelector(".prepend-slide")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper.prependSlide(
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-    );
-  });
-document
-  .querySelector(".append-slide")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper.appendSlide(
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-    );
-  });
-document
-  .querySelector(".append-2-slides")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper.appendSlide([
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-    ]);
-  });
-
 
 
 // Haikal Ihza bertanggung jawab untuk bagian TV Show
