9047a18f2dd2abfafedf0a6bec050b57b020da25
collegeid <155450542+collegeid@users.noreply.github.com>
2024-05-20 23:19:22 +0700

    Revert "memindahkan syntax css dan js pada main ke file music css dan js"

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,53 @@
 
 // Siswo Adi Nugroho bertanggung jawab untuk bagian Music
 // Branch: MUSIC
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: '-28',
+  speed:1200,
+  // autoplay: 
+  //   {
+  //     delay: 1000,
+  //   },
+  loop: true,
+  zoom: true,
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 80,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.addEventListener("click",function (event){
+  const musicCardOverlay = event.target.closest('#cards-music');
+  const closeBtn = event.target.closest('.close-btn');
+  const modalDialog = event.target.closest('#music-modal-dialog');
+  const musicPopup = document.querySelector('#music-modal');
+  const musicImg = document.querySelector('#music-modal img');
+  
+  //GET DATA
+  let getImg
+  if(musicCardOverlay) getImg = musicCardOverlay.querySelector('img').getAttribute('src');
+
+  
+    // Membuka Popup
+    if (musicCardOverlay) {
+      event.preventDefault();
+      musicPopup.style.display = 'flex';
+      musicImg.src = getImg 
+    } else if (closeBtn || !modalDialog) {
+      musicPopup.style.display = 'none';
+    }
+  })
 
 // Haikal Ihza bertanggung jawab untuk bagian TV Show
 // Branch: TV
