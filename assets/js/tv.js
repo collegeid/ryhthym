04f4c6d65a97af13b81e3db6a31c4fc18c90dd4b
haikalananta25 <haikalananta25>
2024-05-20 21:51:45 +0700

    ceking pull main and creat popup tv

--- assets/js/tv.js
+++ assets/js/tv.js
@@ -22,3 +22,39 @@
    .catch(error => {
      console.error('Error fetching data:', error);
    });
+
+   // js popup haikal */
+
+   var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  speed: 1200,
+  autoplay: 
+{
+   delay: 2000,
+  },
+  loop: true,
+  zoom: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+  
+});
