b23a041fa36cb826d671bbd734c7c693af31d7f6
febrd <halo@febrian.id>
2024-05-26 15:48:58 +0700

    music endpoint

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,96 +1,70 @@
-// Mengambil data dari API
-fetch('https://antares.gigalixirapp.com/lite/cinema/movies')
-  .then(response => response.json())
-  .then(data => {
-    // Logging data yang diterima dari API pertama
+// Fungsi async untuk mengambil dan menampilkan data film
+async function fetchAndDisplayMovies() {
+  try {
+    const response = await fetch('https://antares.gigalixirapp.com/lite/cinema/movies');
+    const data = await response.json();
     console.log('Data film:', data);
 
-    // Mengambil array hasil dari data
     const results = data.movies;
-
-    // Membuat variabel untuk menampung markup HTML untuk daftar film
     let html = '';
-    const modalPromises = [];
+    const modalHtmlArray = [];
 
-    // Perulangan melalui setiap objek hasil
-    results.forEach(movie => {
-      // Membuat markup untuk setiap film
+    for (const movie of results) {
+      console.log('Processing film:', movie);
       html += `
       <div class="swiper-slide" onclick="openModal('myModal${movie.id}', '${movie.title}', '${movie.overview}', '${movie.popularity}', '${movie.vote_count}')"><img src="${movie.poster_path}" alt="${movie.title}"></div>
       `;
 
-      // Logging setiap film yang sedang diproses
-      console.log('Processing film:', movie);
+      try {
+        const detailResponse = await fetch(`https://antares.gigalixirapp.com/movies/${movie.id}`);
+        const detail = await detailResponse.json();
+        console.log(`Detail untuk film ID ${movie.id}:`, detail);
+        const video = detail.videos.results.find(video => video.site === "YouTube");
+        const videoUrl = video ? `https://www.youtube.com/embed/${video.key}` : '';
 
-      // Mengambil detail film dari API kedua menggunakan ID film
-      const modalPromise = fetch(`https://antares.gigalixirapp.com/movies/${movie.id}`)
-        .then(response => response.json())
-        .then(detail => {
-          // Logging data detail film yang diterima dari API kedua
-          console.log(`Detail untuk film ID ${movie.id}:`, detail);
-          const video = detail.videos.results.find(video => video.site === "YouTube");
-          const videoUrl = video ? `https://www.youtube.com/embed/${video.key}` : '';
-
-          // Mengembalikan modal HTML untuk setiap film
-          return `
-          <div id="myModal${movie.id}" class="modal-film" onclick="closeModal(event)">
-            <span class="close">&times;</span>
-            <div class="modal-content-film">
-              <div class="posisi-kiri">
-                <img id="myposter-film" src="${movie.poster_path}" alt="${detail.title}">
-                ${videoUrl ? `
-                <div class="play-btn" onclick="openVideoPopup('${videoUrl}')">
-                  <i class="fa fa-play-circle"></i>
-                </div>` : ''}
+        modalHtmlArray.push(`
+        <div id="myModal${movie.id}" class="modal-film" onclick="closeModal(event)">
+        <span class="close">&times;</span>
+        <div class="modal-content-film">
+          <div class="posisi-kiri">
+            <img id="myposter-film" src="${movie.poster_path}" alt="${detail.title}">
+            ${videoUrl ? `
+            <div class="play-btn" onclick="openVideoPopup('${videoUrl}')">
+              <i class="fa fa-play-circle"></i>
+            </div>` : ''}
+          </div>
+          <div class="posisi-kanan">
+            <h1>${detail.title}</h1>
+            <p class="sinopsis-film">${detail.overview}</p>
+            <div class="kotak-rating-film">
+             <!-- <i class="views-film"><i class="fas fa-eye"></i> ${detail.popularity}</i> -->
+              <div class="like-btn" onclick="toggleLikeFilm()">
+                <i id="like-icon-film" class="far fa-heart"></i>
+                <span id="like-count-film">${detail.vote_count}</span>
               </div>
-              <div class="posisi-kanan">
-                <h1>${detail.title}</h1>
-                <p class="sinopsis-film">${detail.overview}</p>
-                <div class="kotak-rating-film">
-                 <!-- <i class="views-film"><i class="fas fa-eye"></i> ${detail.popularity}</i> -->
-                  <div class="like-btn" onclick="toggleLikeFilm()">
-                    <i id="like-icon-film" class="far fa-heart"></i>
-                    <span id="like-count-film">${detail.vote_count}</span>
-                  </div>
-                  <i class="rating-film"><span class="star"></span> ${detail.vote_average} </i>
-                </div>
-              </div>  
+              <i class="rating-film"><span class="star"></span> ${detail.vote_average} </i>
             </div>
-          </div>`;
-        })
-        .catch(error => {
-          console.error(`Error fetching movie details for movie ID ${movie.id}:`, error);
-        });
+          </div>  
+        </div>
+      </div>
+        `);
+      } catch (error) {
+        console.error(`Error fetching movie details for movie ID ${movie.id}:`, error);
+      }
+    }
 
-      modalPromises.push(modalPromise);
-    });
-
-    // Logging HTML yang dihasilkan untuk daftar film
     console.log('HTML untuk daftar film:', html);
-
-    // Memasukkan markup HTML setelah elemen dengan id 'film-data'
     document.getElementById('film-data').insertAdjacentHTML('afterend', html);
+    document.getElementById('film-data').remove();
 
-    // Menghapus elemen dengan id 'film-data' setelah kontennya dimasukkan
-    const filmDataElement = document.getElementById('film-data');
-    filmDataElement.parentNode.removeChild(filmDataElement);
-
-    // Tunggu sampai semua detail permintaan selesai
-    Promise.all(modalPromises).then(modalHtmlArray => {
-      // Menggabungkan semua markup modal HTML
-      const modal_html = modalHtmlArray.join('');
-
-      // Memasukkan markup modal HTML setelah elemen dengan id 'modal-film'
-      document.getElementById('modal-film').insertAdjacentHTML('afterend', modal_html);
-
-      // Menghapus elemen dengan id 'modal-film' setelah kontennya dimasukkan
-      const modalFilmElement = document.getElementById('modal-film');
-      modalFilmElement.parentNode.removeChild(modalFilmElement);
-
-      // Logging modal HTML yang dihasilkan
-      console.log('Modal HTML untuk semua film:', modal_html);
-    });
-  })
-  .catch(error => {
+    const modal_html = modalHtmlArray.join('');
+    document.getElementById('modal-film').insertAdjacentHTML('afterend', modal_html);
+    document.getElementById('modal-film').remove();
+    console.log('Modal HTML untuk semua film:', modal_html);
+  } catch (error) {
     console.error('Error fetching data:', error);
-  });
+  }
+}
+
+// Panggil fungsi untuk menjalankan
+fetchAndDisplayMovies();
--- assets/js/show.js
+++ assets/js/show.js
@@ -1,20 +1,16 @@
-// Mengambil data dari API
-fetch('https://stellar.gigalixirapp.com/lite/cinema/tv')
-  .then(response => response.json())
-  .then(data => {
-    // Logging data yang diterima dari API pertama
+// Fungsi async untuk mengambil dan menampilkan data acara TV
+async function fetchAndDisplayTvShows() {
+  try {
+    const response = await fetch('https://stellar.gigalixirapp.com/lite/cinema/tv');
+    const data = await response.json();
     console.log('Data tv:', data);
 
-    // Mengambil array hasil dari data
     const results = data.tv;
-    
-    // Membuat variabel untuk menampung markup HTML untuk daftar film
     let html = '';
-    const modalPromises = [];
+    const modalHtmlArray = [];
 
-    // Perulangan melalui setiap objek hasil
-    results.forEach(tv_show => {
-      // Membuat markup untuk setiap film
+    for (const tv_show of results) {
+      console.log('Processing tv:', tv_show);
       html += `
         <div class="swiper-slide">
           <img src="${tv_show.poster_path}" class="thumbnail">
@@ -24,79 +20,54 @@ fetch('https://stellar.gigalixirapp.com/lite/cinema/tv')
         </div>
       `;
 
-      // Logging setiap film yang sedang diproses
-      console.log('Processing tv:', tv_show);
-
-      // Mengambil detail film dari API kedua menggunakan ID film
-      const modalPromise = fetch(`https://stellar.gigalixirapp.com/tv/${tv_show.id}`)
-        .then(response => response.json())
-        .then(detail_tv => {
-          // Membuat markup untuk episode
-          let episode_html = '';
-          detail_tv.seasons.episodes.forEach((episode, index) => {
-            episode_html += `<li><button type="button" class="btn btn-secondary">EPISODE ${index + 1}: ${episode.name}</button></li>`;
-          });
-          // Mengembalikan modal HTML dengan episode yang diperbarui
-          return `
-            <div class="modal fade" id="staticBackdrop${tv_show.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <div class="embed-responsive-tv">
-                      <iframe class="embed-responsive-item" src="https://image.tmdb.org/t/p/w500${detail_tv.poster_path}" allowfullscreen width="470px" height="300"></iframe>
-                    </div>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <h2 class="text-episode">Episode Series</h2>
-                    <ul class="episode-list">
-                      ${episode_html}
-                    </ul>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          `;
-        })
-        .catch(error => {
-          console.error(`Error fetching movie details for movie ID ${tv_show.id}:`, error);
+      try {
+        const detailResponse = await fetch(`https://stellar.gigalixirapp.com/tv/${tv_show.id}`);
+        const detail_tv = await detailResponse.json();
+        let episode_html = '';
+        detail_tv.seasons.episodes.forEach((episode, index) => {
+          episode_html += `<li><button type="button" class="btn btn-secondary">EPISODE ${index + 1}: ${episode.name}</button></li>`;
         });
 
-      modalPromises.push(modalPromise);
-    });
+        modalHtmlArray.push(`
+        <div class="modal fade" id="staticBackdrop${tv_show.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <div class="embed-responsive-tv">
+                <iframe class="embed-responsive-item" src="https://image.tmdb.org/t/p/w500${detail_tv.poster_path}" allowfullscreen width="470px" height="300"></iframe>
+              </div>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <h2 class="text-episode">Episode Series</h2>
+              <ul class="episode-list">
+                ${episode_html}
+              </ul>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+        `);
+      } catch (error) {
+        console.error(`Error fetching tv show details for tv show ID ${tv_show.id}:`, error);
+      }
+    }
 
-    // Logging HTML yang dihasilkan untuk daftar film
     console.log('HTML untuk daftar tv:', html);
-
-    // Memasukkan markup HTML setelah elemen dengan id 'tv-data'
     document.getElementById('tv-data').insertAdjacentHTML('afterend', html);
+    document.getElementById('tv-data').remove();
 
-    // Menghapus elemen dengan id 'tv-data' setelah kontennya dimasukkan
-    const tvDataElement = document.getElementById('tv-data');
-      tvDataElement.parentNode.removeChild(tvDataElement);
-
-
-    // Tunggu sampai semua detail permintaan selesai
-    Promise.all(modalPromises).then(modalHtmlArray => {
-      // Menggabungkan semua markup modal HTML
-      const modal_html = modalHtmlArray.join('');
-
-      // Logging modal HTML yang dihasilkan
-
-      // Memasukkan markup modal HTML setelah elemen dengan id 'modal-tv'
-      document.getElementById('modal-tv').insertAdjacentHTML('afterend', modal_html);
-
-      // Menghapus elemen dengan id 'modal-tv' setelah kontennya dimasukkan
-      const modalTvElement = document.getElementById('modal-tv');
-      modalTvElement.parentNode.removeChild(modalTvElement);
-
-      console.log('Modal HTML untuk semua tv:', modal_html);
-
-    });
-  })
-  .catch(error => {
+    const modal_html = modalHtmlArray.join('');
+    document.getElementById('modal-tv').insertAdjacentHTML('afterend', modal_html);
+    document.getElementById('modal-tv').remove();
+    console.log('Modal HTML untuk semua tv:', modal_html);
+  } catch (error) {
     console.error('Error fetching data:', error);
-  });
+  }
+}
+
+// Panggil fungsi untuk menjalankan
+fetchAndDisplayTvShows();
